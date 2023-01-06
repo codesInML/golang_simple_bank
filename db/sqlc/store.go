807e1ec7a05d746de1c8a93ae2d30161e08c856a
Ifeoluwa Olubo <ifeoluwaolubo@gmail.com>
2023-01-07 00:37:12 +0100

    started working on trasactions and fixed transfer test

---
+++ db/sqlc/store.go
@@ -0,0 +1,75 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// store provides all functions to execute db queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// NewStore creates a new store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// execTx executes a function within a database transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbError := tx.Rollback(); rbError != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbError)
+		}
+
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"fromAccountID"`
+	ToAccountID   int64 `json:"toAccountID"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"fromAccount"`
+	ToAccount   Account  `json:"toAccount"`
+	FromEntry   Entry    `json:"fromEntry"`
+	ToEntry     Entry    `json:"toEntry"`
+}
+
+// TransferTx performs a money transfer from one account to another
+// It creates a transfer record, add account entries and update accounts' balance within a single database transaction
+func (store *Store) TransferTx(ctx context.Context, args TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams(args))
+
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.cre
+		return nil
+	})
+
+	return result, err
+}
--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -2,14 +2,16 @@ package db
 
 import (
 	"context"
-	"database/sql"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateTransfer(t *testing.T) {
-	arg := CreateTransferParams{FromAccountID: sql.NullInt64{Int64: 1}, ToAccountID: sql.NullInt64{Int64: 2}, Amount: 50}
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	arg := CreateTransferParams{FromAccountID: account1.ID, ToAccountID: account2.ID, Amount: 50}
 
 	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
 
