83e03e7fd7cfa6f27e8d8a21b1903acf7ce3d62e
Ifeoluwa Olubo <ifeoluwaolubo@gmail.com>
2023-01-13 21:33:18 +0100

    opitimized update account balance query

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -88,30 +88,18 @@ func (store *Store) TransferTx(ctx context.Context, args TransferTxParams) (Tran
 
 		// update accounts' balance
 
-		account1, err := q.GetAccountForUpdate(context.Background(), args.FromAccountID)
-
-		if err != nil {
-			return err
-		}
-
-		result.FromAccount, err = q.UpdateAccount(context.Background(), UpdateAccountParams{
-			ID:      args.FromAccountID,
-			Balance: account1.Balance - args.Amount,
+		result.FromAccount, err = q.AddAccountBalance(context.Background(), AddAccountBalanceParams{
+			ID:     args.FromAccountID,
+			Amount: -args.Amount,
 		})
 
 		if err != nil {
 			return err
 		}
 
-		account2, err := q.GetAccountForUpdate(context.Background(), args.ToAccountID)
-
-		if err != nil {
-			return err
-		}
-
-		result.ToAccount, err = q.UpdateAccount(context.Background(), UpdateAccountParams{
-			ID:      args.ToAccountID,
-			Balance: account2.Balance + args.Amount,
+		result.ToAccount, err = q.AddAccountBalance(context.Background(), AddAccountBalanceParams{
+			ID:     args.ToAccountID,
+			Amount: args.Amount,
 		})
 
 		if err != nil {
