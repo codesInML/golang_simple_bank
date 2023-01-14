e16a563627166e57493eeb54718469c81abac66e
Ifeoluwa Olubo <ifeoluwaolubo@gmail.com>
2023-01-14 23:46:42 +0100

    added viper for loading environment variables

--- main.go
+++ main.go
@@ -6,24 +6,23 @@ import (
 
 	"github.com/codesInML/simplebank/api"
 	db "github.com/codesInML/simplebank/db/sqlc"
+	"github.com/codesInML/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secretPassword1234@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server:", err)
